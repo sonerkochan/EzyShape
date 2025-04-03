b16b2f9dfb80f1ef5ec9d171544fd0fcfe6ed3cf
Soner Kochan <s216253@stud.uni-ruse.bg>
2025-04-03 11:55:47 +0300

    Added sub navigation on Training menu of Clients

---
+++ EzyShape/wwwroot/js/toggleTrainingSubNavigation.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".suv-nav-button");
+    const programsDiv = document.querySelector(".programs-div");
+    const historyDiv = document.querySelector(".history-div");
+
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", function () {
+            document.querySelector(".sub-active")?.classList.remove("sub-active");
+            this.classList.add("sub-active");
+
+            if (index === 0) {
+                programsDiv.classList.add("div-active");
+                historyDiv.classList.remove("div-active");
+            } else {
+                historyDiv.classList.add("div-active");
+                programsDiv.classList.remove("div-active");
+            }
+        });
+    });
+});
