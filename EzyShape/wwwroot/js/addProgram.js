d2b00bf2d4abeac21fe16a918dd32e07c36dc257
Soner Kochan <s216253@stud.uni-ruse.bg>
2025-04-04 19:00:17 +0300

    Programs/Add Program page UI improvements

---
+++ EzyShape/wwwroot/js/addProgram.js
@@ -0,0 +1,16 @@
+﻿document.getElementById("add-program").onclick = function () {
+    document.getElementById("addProgramModal").style.display = "flex";
+};
+
+document.getElementById("closeModalBtn").onclick = closeModal;
+
+function closeModal() {
+    document.getElementById("addProgramModal").style.display = "none";
+}
+
+window.onclick = function (event) {
+    const modal = document.getElementById("addProgramModal");
+    if (event.target === modal) {
+        closeModal();
+    }
+};
