87324f006deed90d1759383800dc1a79ac928cd4
Soner Kochan <s216253@stud.uni-ruse.bg>
2025-06-15 11:24:44 +0300

    feat: Share Access option added

---
+++ EzyShape/wwwroot/js/Trainer/shareAccessModal.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("accessModal");
+    const button = document.querySelector(".button-access");
+    const closeBtn = document.querySelector(".close-button");
+
+    button.addEventListener("click", function () {
+        modal.style.display = "flex";
+    });
+
+    closeBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (e) {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
